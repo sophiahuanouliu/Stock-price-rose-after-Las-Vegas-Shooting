16c35ecc67f2aab5c3766a3c25d75dd015fbb29a
Sophia Liu <hliu46@stevens.edu>
2017-10-05 13:51:10 -0400

    Add files via upload

---
+++ Massive Shooting/Las Vegas/figure.js
@@ -0,0 +1,169 @@
+var figure = {
+    "frames": [], 
+    "layout": {
+        "autosize": false, 
+        "yaxis": {
+            "range": [
+                -0.024407813833578845, 
+                0.045497502837998034
+            ], 
+            "type": "linear", 
+            "autorange": true, 
+            "ticksuffix": "%", 
+            "title": "Daily Returns"
+        }, 
+        "title": "<b>Las Vegas Massive Shooting on Oct 1, 2017</b>", 
+        "showlegend": true, 
+        "height": 500, 
+        "width": 1000, 
+        "xaxis": {
+            "tickfont": {
+                "size": 10
+            }, 
+            "range": [
+                0, 
+                6.203961105005143
+            ], 
+            "type": "category", 
+            "autorange": true, 
+            "title": "Date"
+        }, 
+        "hovermode": "closest", 
+        "breakpoints": [], 
+        "annotations": [
+            {
+                "xref": "x", 
+                "yref": "y", 
+                "text": "<i>&nbsp;</i><i>Gun Stocks Jump After Las Vegas Massive Shooting</i>&nbsp;", 
+                "ay": -30, 
+                "ax": -39, 
+                "y": 0.03675933825405092, 
+                "x": 4.994557972928248, 
+                "font": {
+                    "color": "rgb(214, 39, 40)", 
+                    "family": "Arial", 
+                    "size": 14
+                }, 
+                "showarrow": true
+            }
+        ]
+    }, 
+    "data": [
+        {
+            "opacity": 0.40000000000000013, 
+            "autobinx": true, 
+            "uid": "a84004", 
+            "ysrc": "zhangr03:8:623a84", 
+            "xsrc": "zhangr03:8:8048d5", 
+            "name": "S&amp;P 500", 
+            "mode": "lines", 
+            "y": [
+                "-0.000883002", 
+                "0.000602579", 
+                "0.003894331", 
+                "0.00119976", 
+                "0.003515079", 
+                "0.004338654", 
+                "0.00214014"
+            ], 
+            "x": [
+                "9/25/2017", 
+                "9/26/2017", 
+                "9/27/2017", 
+                "9/28/2017", 
+                "9/29/2017", 
+                "10/2/2017", 
+                "10/3/2017"
+            ], 
+            "line": {
+                "width": 7
+            }, 
+            "type": "scatter", 
+            "autobiny": true
+        }, 
+        {
+            "autobinx": true, 
+            "uid": "d5a568", 
+            "ysrc": "zhangr03:8:bd5aaa", 
+            "xsrc": "zhangr03:8:8048d5", 
+            "name": "American Outdoor Brands Corp", 
+            "mode": "lines", 
+            "y": [
+                "-0.008414239", 
+                "0.005874674", 
+                "0.024010383", 
+                "-0.020912548", 
+                "-0.012944984", 
+                "0.032131148", 
+                "0.024142313"
+            ], 
+            "x": [
+                "9/25/2017", 
+                "9/26/2017", 
+                "9/27/2017", 
+                "9/28/2017", 
+                "9/29/2017", 
+                "10/2/2017", 
+                "10/3/2017"
+            ], 
+            "type": "scatter", 
+            "autobiny": true
+        }, 
+        {
+            "autobinx": true, 
+            "uid": "ab1687", 
+            "ysrc": "zhangr03:8:31790b", 
+            "xsrc": "zhangr03:8:8048d5", 
+            "name": "Sturm Ruger &amp; Company Inc", 
+            "mode": "lines", 
+            "y": [
+                "0.008763389", 
+                "-0.001930502", 
+                "0.02901354", 
+                "-0.012218045", 
+                "-0.016175071", 
+                "0.034816248", 
+                "0.021495327"
+            ], 
+            "x": [
+                "9/25/2017", 
+                "9/26/2017", 
+                "9/27/2017", 
+                "9/28/2017", 
+                "9/29/2017", 
+                "10/2/2017", 
+                "10/3/2017"
+            ], 
+            "type": "scatter", 
+            "autobiny": true
+        }, 
+        {
+            "autobinx": true, 
+            "uid": "8e109d", 
+            "ysrc": "zhangr03:8:4d19fd", 
+            "xsrc": "zhangr03:8:8048d5", 
+            "name": "Vista Outdoor Inc", 
+            "mode": "lines", 
+            "y": [
+                "0.003030303", 
+                "-0.007768666", 
+                "0.016963897", 
+                "-0.015397776", 
+                "-0.003475239", 
+                "0.024411508", 
+                "-0.014893617"
+            ], 
+            "x": [
+                "9/25/2017", 
+                "9/26/2017", 
+                "9/27/2017", 
+                "9/28/2017", 
+                "9/29/2017", 
+                "10/2/2017", 
+                "10/3/2017"
+            ], 
+            "type": "scatter", 
+            "autobiny": true
+        }
+    ]
+}
---
+++ Massive Shooting/Orlando Night Club/figure.js
@@ -0,0 +1,228 @@
+var figure = {
+    "frames": [], 
+    "layout": {
+        "autosize": false, 
+        "yaxis": {
+            "domain": [
+                0.07, 
+                0.98
+            ], 
+            "title": "Daily Returns", 
+            "range": [
+                -0.061946094907536074, 
+                0.12156682324318548
+            ], 
+            "ticksuffix": "%", 
+            "type": "linear", 
+            "autorange": true
+        }, 
+        "plot_bgcolor": "rgb(255, 255, 255)", 
+        "title": "<b>Orlando Nightclub&nbsp;Massive Shooting on&nbsp;June 12, 2016</b>", 
+        "showlegend": true, 
+        "height": 500, 
+        "width": 1000, 
+        "xaxis": {
+            "tickfont": {
+                "size": 10
+            }, 
+            "domain": [
+                0, 
+                0.98
+            ], 
+            "title": "Date", 
+            "range": [
+                0, 
+                9
+            ], 
+            "type": "category", 
+            "autorange": true
+        }, 
+        "hovermode": "closest", 
+        "breakpoints": [], 
+        "annotations": [
+            {
+                "xref": "x", 
+                "arrowhead": 3, 
+                "arrowsize": 1.5000000000000004, 
+                "yref": "y", 
+                "text": "<i>Huge jump of gun makers' stocks after the massive shooting</i>", 
+                "ay": -41, 
+                "ax": -2, 
+                "y": 0.08848156155735827, 
+                "x": 4.980451127819549, 
+                "font": {
+                    "color": "rgb(214, 39, 40)", 
+                    "size": 14
+                }, 
+                "showarrow": true
+            }
+        ]
+    }, 
+    "data": [
+        {
+            "opacity": 0.40000000000000013, 
+            "showlegend": true, 
+            "autobinx": true, 
+            "uid": "ebe5ab", 
+            "yaxis": "y", 
+            "ysrc": "zhangr03:3:81df29", 
+            "connectgaps": false, 
+            "xsrc": "zhangr03:3:958b9f", 
+            "name": "S&amp;P 500", 
+            "mode": "lines", 
+            "xaxis": "x", 
+            "y": [
+                "0.003084955", 
+                "0.001561391", 
+                "0.003259637", 
+                "-0.001365541", 
+                "-0.009477556", 
+                "-0.007711715", 
+                "-0.001966899", 
+                "-0.001393963", 
+                "0.002984356", 
+                "-0.008878437"
+            ], 
+            "x": [
+                "6/6/16", 
+                "6/7/16", 
+                "6/8/16", 
+                "6/9/16", 
+                "6/10/16", 
+                "6/13/16", 
+                "6/14/16", 
+                "6/15/16", 
+                "6/16/16", 
+                "6/17/16"
+            ], 
+            "line": {
+                "dash": "solid", 
+                "color": "rgb(31, 119, 180)", 
+                "width": 7
+            }, 
+            "type": "scatter", 
+            "autobiny": true
+        }, 
+        {
+            "showlegend": true, 
+            "autobinx": true, 
+            "uid": "ad64b8", 
+            "ysrc": "zhangr03:3:2614ce", 
+            "connectgaps": false, 
+            "xsrc": "zhangr03:3:958b9f", 
+            "name": "American Outdoor Brands Corp", 
+            "mode": "lines", 
+            "y": [
+                "-0.052770449", 
+                "-0.000928505", 
+                "0.009758364", 
+                "0.012885412", 
+                "-0.027260336", 
+                "0.068659505", 
+                "-0.04458042", 
+                "-0.017383349", 
+                "0.012569832", 
+                "0.087356322"
+            ], 
+            "x": [
+                "6/6/16", 
+                "6/7/16", 
+                "6/8/16", 
+                "6/9/16", 
+                "6/10/16", 
+                "6/13/16", 
+                "6/14/16", 
+                "6/15/16", 
+                "6/16/16", 
+                "6/17/16"
+            ], 
+            "line": {
+                "dash": "solid", 
+                "width": 2
+            }, 
+            "type": "scatter", 
+            "autobiny": true
+        }, 
+        {
+            "showlegend": true, 
+            "autobinx": true, 
+            "uid": "a9c858", 
+            "ysrc": "zhangr03:3:da4fe3", 
+            "connectgaps": false, 
+            "xsrc": "zhangr03:3:958b9f", 
+            "name": "Sturm Ruger &amp; Company Inc", 
+            "mode": "lines", 
+            "y": [
+                "-0.038165491", 
+                "-0.01417139", 
+                "0.00913242", 
+                "-0.002849003", 
+                "-0.03512605", 
+                "0.085002613", 
+                "-0.041900787", 
+                "-0.016253351", 
+                "0.012263669", 
+                "0.020360087"
+            ], 
+            "x": [
+                "6/6/16", 
+                "6/7/16", 
+                "6/8/16", 
+                "6/9/16", 
+                "6/10/16", 
+                "6/13/16", 
+                "6/14/16", 
+                "6/15/16", 
+                "6/16/16", 
+                "6/17/16"
+            ], 
+            "line": {
+                "dash": "solid", 
+                "color": "rgb(148, 103, 189)", 
+                "width": 2
+            }, 
+            "type": "scatter", 
+            "autobiny": true
+        }, 
+        {
+            "showlegend": true, 
+            "autobinx": true, 
+            "uid": "d5cf7e", 
+            "ysrc": "zhangr03:3:de4286", 
+            "connectgaps": false, 
+            "xsrc": "zhangr03:3:958b9f", 
+            "name": "Vista Outdoor Inc", 
+            "mode": "lines", 
+            "y": [
+                "-0.02678028", 
+                "-0.001250782", 
+                "-0.005635567", 
+                "0.001049538", 
+                "-0.013629692", 
+                "0.001913265", 
+                "0.00466794", 
+                "0.004435058", 
+                "-0.010513036", 
+                "0.012112197"
+            ], 
+            "x": [
+                "6/6/16", 
+                "6/7/16", 
+                "6/8/16", 
+                "6/9/16", 
+                "6/10/16", 
+                "6/13/16", 
+                "6/14/16", 
+                "6/15/16", 
+                "6/16/16", 
+                "6/17/16"
+            ], 
+            "line": {
+                "dash": "solid", 
+                "width": 2
+            }, 
+            "type": "scatter", 
+            "autobiny": true
+        }
+    ]
+}
